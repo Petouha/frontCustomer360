d3d346319597c541a52a3fa707c0cdd4f878fd09
Petouha <rachidgames2.rg@gmail.com>
2023-05-29 15:25:14 +0100

    the data is successfully displayed

--- frontCustomer/src/assets/components/Principal/principal.jsx
+++ frontCustomer/src/assets/components/Principal/principal.jsx
@@ -6,7 +6,7 @@
 
   const Principal = () => {
     const [searchTerm, setSearchTerm] = useState('');
-    const [customerDetails, setCustomerDetails] = useState(null);
+    const [customerDetails, setCustomerDetails] = useState([]);
     const [customerBehavior, setCustomerBehavior] = useState([]);
     const [customerHistory, setCustomerHistory] = useState([]);
 
@@ -25,6 +25,7 @@
     const fetchCustomerBehavior = async (customerId) => {
       try {
         const response = await axios.get(`http://localhost:8000/api/v1/users/${customerId}`);
+        console.log(response);
         setCustomerBehavior(response.data.subscriber_behaviour[0]);
       } catch (error) {
         console.log(error);
@@ -34,9 +35,9 @@
     // Fonction pour récupérer l'historique du client à partir de l'API
     const fetchCustomerHistory = async (customerId) => {
       try {
-        const response = await axios.get(`http://localhost:8000/api/v1/users/${customerId}`);
+        const response = await axios.get(`http://localhost:8000/api/v1/users/${searchTerm}`);
         console.log(response.data.history);
-        setCustomerHistory(response.data.history[0]);
+        setCustomerHistory(response.data.history);
       } catch (error) {
         console.log(error);
       }
@@ -44,9 +45,9 @@
 
     // Effet pour récupérer les détails du client au chargement de la page
     useEffect(() => {
-      fetchCustomerDetails(1);
-      fetchCustomerBehavior(1);
-      fetchCustomerHistory(1);
+      fetchCustomerDetails(searchTerm);
+      fetchCustomerBehavior(searchTerm);
+      fetchCustomerHistory(searchTerm);
     }, []);
 
     // Fonction pour gérer la soumission du formulaire de recherche
@@ -59,79 +60,83 @@
         fetchCustomerDetails(customer.subscriber_info[0].MSISDN);
         fetchCustomerBehavior(customer.subscriber_info[0].MSISDN);
         fetchCustomerHistory(customer.subscriber_info[0].MSISDN);
-        console.log(response.data);
+        console.log(customerHistory);
       } catch (error) {
         console.log(error);
       }
     };
 
     return (
-      <div>    <img src="https://scontent.cdninstagram.com/v/t1.15752-9/293558223_1184855078914133_5227506387825766741_n.png?stp=dst-png_s320x320&_nc_cat=104&ccb=1-7&_nc_sid=5a057b&_nc_ohc=-7KlQnO2PJ4AX_wtQ_f&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AdS6sqoGtonY6PAC8BALoU8O0aYDNaDKU9sJ91phUN2uGg&oe=648DE044" alt="Description de l'image" className="photo-profil"></img>
+      <div>    <img src="https://scontent.cdninstagram.com/v/t1.15752-9/293558223_1184855078914133_5227506387825766741_n.png?stp=dst-png_s320x320&_nc_cat=104&ccb=1-7&_nc_sid=5a057b&_nc_ohc=-7KlQnO2PJ4AX_wtQ_f&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AdS6sqoGtonY6PAC8BALoU8O0aYDNaDKU9sJ91phUN2uGg&oe=648DE044" alt="Description de l'image" class="photo-profil"></img>
         <div id="container">
           <div id="search-form">
             <form onSubmit={handleSearch}>
-              <div className="search-container">
+              <div class="search-container">
                 <input type="text" id="search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Entrer un numéro" />
-                <span className="search-icon"><FontAwesomeIcon icon={faSearch} /></span>
+                <span class="search-icon"><FontAwesomeIcon icon={faSearch} /></span>
               </div>
 
             </form>
           </div>
 
-          <div className="section-container">
-            <div id="customer-details" className="section white-box">
+          <div class="section-container">
+            <div id="customer-details" class="section white-box">
               <h2>Customer Details</h2>
               <p><strong>Nom complet :</strong> {customerDetails.fullName || "NaN"}</p>
-              {/* <p><strong>Téléphone :</strong> {customerDetails.phone}</p>
-              <p><strong>Wilaya :</strong> {customerDetails.city}</p>
-              <p><strong>Age :</strong> {customerDetails.age}</p>
-              <p><strong>Ancienneté :</strong> {customerDetails.seniority}</p>
+              <p><strong>Téléphone :</strong> {customerDetails.MSISDN}</p>
+              <p><strong>Wilaya :</strong> {customerDetails.wilaya}</p>
+              <p><strong>Age :</strong> {customerDetails.Age}</p>
+              <p><strong>Ancienneté :</strong> {customerDetails.Seniority}</p>
               <p><strong>NPS :</strong> {customerDetails.nps}</p>
-              <p><strong>Sexe :</strong> {customerDetails.gender}</p>
-              <p><strong>Souscription :</strong> {customerDetails.subscription}</p>
-              <p><strong>Réseau :</strong> {customerDetails.network}</p>
+              <p><strong>Sexe :</strong> {customerDetails.sex}</p>
+              <p><strong>Souscription :</strong> {customerDetails.Subscriptions_Type}</p>
+              <p><strong>Réseau :</strong> {customerDetails.lineType}</p>
               <p><strong>Type SIM :</strong> {customerDetails.simType}</p>
-              <p><strong>Etat :</strong> {customerDetails.status}</p> */}
+              <p><strong>Etat :</strong> {customerDetails.status}</p>
             </div>
 
 
 
 
 
-            <div id="customer-behavior" className="section white-box">
+            <div id="customer-behavior" class="section white-box">
               <h2>Customer Behavior</h2>
-              <div className="customer-behavior-details">
-                {/* <p><strong>Balance :</strong> {customerBehavior.balance}</p>
-                <p><strong>Segment :</strong> {customerBehavior.segment}</p>
-                <p><strong>Comportement :</strong> {customerBehavior.comportement}</p>
+              <div class="customer-behavior-details">
+                <p><strong>Balance :</strong> {customerBehavior.balance}</p>
+                <p><strong>Segment :</strong> {customerBehavior.valueSegment}</p>
+                <p><strong>Comportement :</strong> {customerBehavior.behaviorSegments}</p>
                 <p><strong>Détails :</strong> {customerBehavior.details}</p>
-                <p><strong>Epaiment :</strong> {customerBehavior.epaiment}</p> */}
+                <p><strong>Epaiment :</strong> {customerBehavior.epaiment}</p>
               </div>
               {/* <ul>
-                {customerBehavior.map((item) => (
+                {customerHistory.map((item) => (
                   <li key={item.id}>{item.action} - {item.date}</li>
                 ))}
               </ul> */}
             </div>
-            <div id="customer-history" className="section white-box">
+            <div id="customer-history" class="section white-box">
               <h2>Customer History</h2>
               <table>
                 <thead>
                   <tr>
                     <th>Date</th>
-                    <th>Transaction</th>
-                    <th>Montant</th>
+                    <th>Forfait</th>
                   </tr>
                 </thead>
-                {/* <tbody>
-                  {customerBehavior.map((item) => (
-                    <tr key={item.id}>
-                      <td>{item.date}</td>
-                      <td>{item.action}</td>
-                      <td>{item.montant}</td>
+                <tbody>
+                  {customerHistory && customerHistory.length > 0 ? (
+                    customerHistory.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.dateActivation}</td>
+                        <td>{item.commercialName}</td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="2">No customer history available.</td>
                     </tr>
-                  ))}
-                </tbody> */}
+                  )}
+                </tbody>
               </table>
             </div>
           </div>
