e99a3328d64e23bc123443b60b62de7596943926
GuadjiDounia <douniaguadji@gmail.com>
2023-06-03 15:40:07 +0100

    profil

--- frontCustomer/src/assets/components/Profil/profil.jsx
+++ frontCustomer/src/assets/components/Profil/profil.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './profil.css';
-
-
-
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
@@ -11,10 +7,38 @@ const UserPage = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // Simulating API call with static data
     const fetchUser = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/v1/users');
-        setUser(response.data);
+        // Replace this with your static user data
+        const staticUser = {
+          logo: 'path/to/userLogo.png',
+          photo: 'path/to/userPhoto.png',
+          totalAppel: 10,
+          migration: 5,
+          activation: 3,
+          AppelReport: [
+            {
+              id: 1,
+              action: 'migration',
+              date: '2023-05-02',
+              name: 'John Doe',
+              montant: 2
+            },
+            {
+              id: 1,
+              action: 'migration',
+              date: '2023-05-02',
+              name: 'John Doe',
+              montant: 2 
+            }
+          ],
+          name: 'John Doe',
+          lastLogin: '2023-05-03',
+          isLoggedIn: true
+        };
+
+        setUser(staticUser);
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -35,44 +59,58 @@ const UserPage = () => {
   }
 
   return (
-    <div>
-      <div>
-        <img src={user.png} alt="User Logo" />
-        <img src={user.png} alt="User Photo" />
+    <div className="container">
+      <div className="side-section">
+        <img src={user.logo} alt="User Logo" />
+        <div className="labels">
+          <p>Profil</p>
+          <p>Stats</p>
+          <p>Rapport Agent</p>
+        </div>
       </div>
-      <h1>Statistique </h1>
-      <p>Activation: {user.activation}</p>
-      <p>Migration: {user.migration}</p>
-
-      <h1>rapport agent</h1>
-      <ul>
-        {user.AppelReport.map((report) => (
-          <li key={report.id}>
-            <p>Date: {report.date}</p>
-            <p>Migration: {report.migration}</p>
-            <p>Date: {report.date}</p>
-            <p>Activation: {report.activation}</p>
-          </li>
-        ))}
-      </ul>
-      <h1>User Table</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Matricule</th>
-            <th>Last Login</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{user.name}</td>
-            <td>{user.lastLogin}</td>
-            <td>{user.isLoggedIn ? 'Connected' : 'Not Connected'}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="main-section">
+        <h1>User Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Last Login</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{user.name}</td>
+              <td>{user.lastLogin}</td>
+              <td>{user.isLoggedIn ? 'Connected' : 'Not Connected'}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h1>Statistique</h1>
+        <p>Activation: {user.activation}</p>
+        <p>Migration: {user.migration}</p>
+      
+        <h1>Rapport Agent</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Action</th>
+              <th>Date</th>
+              <th>nom client</th>
+              <th>montant</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.AppelReport.map((report) => (
+              <tr key={report.id}>
+                <td>{report.date}</td>
+                <td>{report.migration}</td>
+                <td>{report.activation}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
