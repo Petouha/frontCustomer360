4301d0ea627014901d9db051195cf37c9a47fcbe
AMIR Farah <farah21.amir@gmail.com>
2023-06-03 19:59:04 +0200

    maj

--- frontCustomer/src/assets/components/Principal/principal.jsx
+++ frontCustomer/src/assets/components/Principal/principal.jsx
@@ -76,7 +76,7 @@ const Principal = () => {
   return (
     <div>
       <Menu />
-      <img src="https://scontent.cdninstagram.com/v/t1.15752-9/293558223_1184855078914133_5227506387825766741_n.png?stp=dst-png_s320x320&_nc_cat=104&ccb=1-7&_nc_sid=5a057b&_nc_ohc=-7KlQnO2PJ4AX_wtQ_f&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AdS6sqoGtonY6PAC8BALoU8O0aYDNaDKU9sJ91phUN2uGg&oe=648DE044" alt="Description de l'image" class="photo-profil"></img>
+      <img src="https://scontent.cdninstagram.com/v/t1.15752-9/293558223_1184855078914133_5227506387825766741_n.png?stp=dst-png_s320x320&_nc_cat=104&ccb=1-7&_nc_sid=5a057b&_nc_ohc=-7KlQnO2PJ4AX_wtQ_f&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AdS6sqoGtonY6PAC8BALoU8O0aYDNaDKU9sJ91phUN2uGg&oe=648DE044" alt="Description de l'image" className="photo-profil"></img>
       <div id="container">
         <div id="search-form">
           <form onSubmit={handleSearch}>
